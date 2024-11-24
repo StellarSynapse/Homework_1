216b0d70b4042c3c7b06bada90c1d2d6a0163d7a
StellarSynapse <skorubskijdmitro@gmail.com>
2024-11-24 16:26:41 +0200

    Add task2 and task3 files

---
+++ task2.py
@@ -0,0 +1,17 @@
+# 'hello world'
+print("hello world")
+
+# 'hello', 'world' with a space separator
+print("hello", "world", sep=" ")
+
+# 'hello', 'world' with a dash separator
+print("hello", "world", sep="-")
+
+# 'hello world' with a newline at the end
+print("hello world", end="\n")
+
+# 'hello world' with '!!!' at the end
+print("hello world", end="!!!\n")
+
+# 'hello' and 'world' with ' ' separator and '!!!' at the end
+print("hello", "world", sep=" ", end="!!!\n")
---
+++ task3.py
@@ -0,0 +1,8 @@
+# Print the letter "O"
+print("#########\n#\t#\n#\t#\n#\t#\n#########")
+
+# Print a newline to separate the letters
+print()
+
+# Print the letter "H"
+print("#\t#\n#\t#\n#########\n#\t#\n#\t#")
